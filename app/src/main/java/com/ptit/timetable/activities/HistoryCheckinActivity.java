beae15a91c0222ea7a92719611be388b848f425a
hieule <leconghieu1998@gmail.com>
2020-07-01 09:08:35 +0700

    merage with master, fix login, profile

--- app/src/main/java/com/ptit/timetable/activities/HistoryCheckinActivity.java
+++ app/src/main/java/com/ptit/timetable/activities/HistoryCheckinActivity.java
@@ -2,7 +2,7 @@ package com.ptit.timetable.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +20,8 @@ import android.view.MenuItem;
 import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.ptit.timetable.adapters.HistoryAdapter;
 import com.ptit.timetable.model.Teacher;
